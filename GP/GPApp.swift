b9253509bd561f0246c58e84436dd858a5881141
wajdAlmansor <wajd2828@gmail.com>
2025-02-24 10:17:45 +0300

    Initial Commit

---
+++ GP/GPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPApp.swift
+//  GP
+//
+//  Created by Wajd on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
